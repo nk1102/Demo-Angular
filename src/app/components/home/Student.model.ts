f409dcf34a169f32a16de456bd0b299fea5ca6b5
Nikunj <nikunjkhandekwal1234@gmail.com>
2023-08-27 20:24:55 +0530

    Change for the code done

---
+++ src/app/components/home/Student.model.ts
@@ -0,0 +1,16 @@
+export class Student{
+
+    public name: string;
+
+    public registerNumber: string;
+
+    public age: number;
+
+    constructor(name: string, registerNumber: string, age: number) {
+        this.name = name;
+        this.registerNumber = registerNumber;
+        this.age = age;
+    }
+
+
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { Student } from './Student.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+
 })
 export class HomeComponent {
 
-  showLogin: boolean = true;
+  student: any[] = [  // Assuming each student is an object with properties name, registerNumber, and age
+  { name: 'John Doe', registerNumber: '12345', age: 18 },
+  { name: 'Jane Smith', registerNumber: '67890', age: 17 }
+  
+];
+
+
 }
