fbff06967c82ec802a5b089f82c25f2ce51e1d63
Nikunj <nikunjkhandekwal1234@gmail.com>
2023-08-29 14:47:30 +0530

    Changed the structure of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { ShowLen } from './app.length';
+import { ProductComponent } from './product/product.component';
 
 
 
@@ -12,9 +12,8 @@ import { ShowLen } from './app.length';
   declarations: [
     
     AppComponent,
-    ContactComponent,
-    HomeComponent,
-    ShowLen
+    
+    ProductComponent
 
    
  
